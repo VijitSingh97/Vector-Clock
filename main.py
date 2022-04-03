78cfee1bb5ae71cd5b6751a504530246aca3c888
Vijit Singh <vijit.singh@aa.com>
2022-04-03 13:16:29 -0500

    add lock around vector counter

--- main.py
+++ main.py
@@ -1,80 +1,99 @@
+from re import T
 from flask import Flask, jsonify, request
-import threading
-import random
-import requests
-import sys
+from logging import ERROR, getLogger
+from random import randint
+from requests import post
+from sys import argv
+from threading import Lock, Thread, Timer
+from time import sleep
 
 # creating a Flask app
 app = Flask(__name__)
   
-# on the terminal type: curl http://127.0.0.1:5000/
-# returns hello world when we use GET.
-# returns the data that we send when we use POST.
-
-# body = {
-#     "counter": [1, 1, 3]
-# }
-
-
-
+# on the terminal type: curl http://localhost:808x/
+# returns the success when we send valid update counter message
+# example body:
+#   body = {
+#       "counter": [1, 1, 3]
+#   }
 @app.route('/', methods = ['POST'])
 def home():
     if(request.method == 'POST'):
         receive_message(request.json)
         return jsonify({'result': 'success'})
 
-
+# method that handles incoming message 
 def receive_message(request):
-    print(f"[{name}] counter before receiving: {counter}" )
-    counter[server_number] += 1
-    for x in range(len(counter)):
-        if request["counter"][x] > counter[x]:
-            counter[x] = request["counter"][x]
-    print(f"[{name}] counter after receiving: {counter}")
-
-def send_message(server_port):
+    lock.acquire()
     try:
-        if (server_port != port):
-            print(f"[{name}] counter before send: {counter}")
-            url = 'http://localhost:' + str(server_port)
-            body = {
-                "counter": counter
-            }
-            requests.post(url, json=body)
-            counter[server_number] += 1
-            print(f"[{name}] counter after send: {counter}")
-    except:
-        pass
+        print(f"[{name}] counter before receiving: {counter}" )
+        counter[server_number] += 1
+        for x in range(len(counter)):
+            if request["counter"][x] > counter[x]:
+                counter[x] = request["counter"][x]
+        print(f"[{name}] counter after receiving: {counter}")
+    finally:
+        lock.release()
 
+# method that sends message to other servers in network
+def send_message(server_port):
+    lock.acquire()
+    try:
+        try:
+            if (server_port != port):
+                print(f"[{name}] counter before send: {counter}")
+                url = 'http://localhost:' + str(server_port)
+                body = {
+                    "counter": counter
+                }
+                post(url, json=body)
+                counter[server_number] += 1
+                print(f"[{name}] counter after send: {counter}")
+        except:
+            pass
+    finally:
+        lock.release()
+
+# method that performs arbitrary event 
 def do_event():
-    print(f"[{name}] counter before event: {counter}")
-    counter[server_number] += 1
-    print(f"[{name}] counter after event: {counter}")
+    lock.acquire()
+    try:
+        print(f"[{name}] counter before event: {counter}")
+        sleep(randint(1,10))
+        counter[server_number] += 1
+        print(f"[{name}] counter after event: {counter}")
+    finally:
+        lock.release()
 
 def generate_name_port():
     port = 8080 + server_number
     name = "server_" + str(port)
     return name, port
 
-def run_events_randomly():
-    threading.Timer(15, run_events_randomly).start()
+def run_events_interval():
+    Timer(randint(1,60), run_events_interval).start()
     do_event()
 
 def run_sends_randomly():
-    threading.Timer(15, run_sends_randomly).start()
-    send_message(8080 + random.randint(0,2))
+    Timer(randint(1,60), run_sends_randomly).start()
+    send_message(8080 + randint(0,2))
 
 if __name__ == '__main__':
-    if len(sys.argv) == 2:
+    if len(argv) == 2:
+        global lock
         global name
         global port
         global server_number
         global counter
-        server_number = int(sys.argv[1])
+        lock = Lock()
+        server_number = int(argv[1])
         name, port = generate_name_port()
         counter = [0, 0, 0]
-        threading.Thread(target=lambda: app.run(host='0.0.0.0', port=port, debug=False, use_reloader=False)).start()
-        run_events_randomly()
+        log = getLogger('werkzeug')
+        log.setLevel(ERROR)
+        Thread(target=lambda: app.run(host='0.0.0.0', port=port, debug=False, use_reloader=False)).start()
+        sleep(1)
+        run_events_interval()
         run_sends_randomly()
     else: 
         print("bad arguments, check readme")
